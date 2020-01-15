c3b9aefc65f473df3ca9a0dbe7ef36cc76f025d2
TeacherXu <1457570681@qq.com>
2020-01-15 21:11:11 +0800

    添加了Filter、Listener、Interceptor

--- src/main/java/com/oasis/springboot/SpringbootApplication.java
+++ src/main/java/com/oasis/springboot/SpringbootApplication.java
@@ -2,7 +2,9 @@ package com.oasis.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.oasis.springboot.action")
 @SpringBootApplication
 public class SpringbootApplication {
 
